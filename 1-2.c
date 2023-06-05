34a10411e92d04f76f7f6b3d00bef2e33e39faa9
Jaehee Jung <wogml414@gmail.com>
2023-06-05 20:55:31 +0900

    6/5

---
+++ 1-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("안녕하세요.");
+	printf("정재희입니다.");
+
+	printf("안녕하세요.\n");
+	printf("정재희입니다.");
+
+	return 0;
+}
---
+++ a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Hello, /* 이것은 주석 아님 */ world.");
+	printf("Hello, world. // 이것도 주석 아님");
+}
